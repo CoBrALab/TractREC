987a1165243dd328c330e16d4aa85b97ef0fda6b
Chris Steele <steelec@gmail.com>
2016-02-04 22:03:19 +0100

    added 'volume' option to help text of extract_q_m

--- TractREC/TractREC.py
+++ TractREC/TractREC.py
@@ -395,7 +395,7 @@ def extract_quantitative_metric(metric_files,label_files,IDs=None,label_df=None,
         - label_df          - pandas dataframe of label index (index) and description (label_id)
         - label_subset_idx  - list of label indices that you want to extract data from [10, 200, 30]
         - label_tag         - string that will precede the label description in the column header
-        - metric            - metric to extract {'mean','median','vox_count'}
+        - metric            - metric to extract {'mean','median','vox_count', 'volume'}
         - thresh_mask_files - list of files for additional thresholding (again, same restrictions as label_files)
         - ROI_mask_files    - binary mask file(s) denoting ROI for extraction =1        
         - thresh_val        - value for threshoding
